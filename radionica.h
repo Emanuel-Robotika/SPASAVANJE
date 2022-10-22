7fc017620a20249e986b7423b7c02b0776ab533f
Emanuel-Robotika <114671890+Emanuel-Robotika@users.noreply.github.com>
2022-10-22 12:15:29 +0200

    221022 update
    Na kraju ne radi skretanje u lijevo

--- radionica.h
+++ radionica.h
@@ -1,17 +1,10 @@
 #pragma once
 #include "helper.h"
 void RobotLine::radionica() {
-  // ZADATAK: uvedite varijablu brojača.
-  // HINT: static int nazivVarijable = vrijednost;
   static int broj = 0;
   static bool prolazak = false;
 
-
-  // VARIJABLA ZA PRETVORBU BROJA U TEKST
   char simbol[10 + sizeof(char)];
-
-  // ZADATAK: skretanje s povećanjem brojača
-  // HINT: prekopirajte iz prijašnjeg zadatka i dodajte brojač
 
   if (setup()) {
     armOpen();
@@ -20,41 +13,69 @@ void RobotLine::radionica() {
   if (broj < 5) {
     if (front() < 150) {
       if (broj == 0 || broj == 3 || broj == 4) {
-        go(100, -100);
-        delayMs(400);
+        go(90, -90);
+        delayMs(500);
+        go(50, 50);
+        delayMs(100);
       } else {
-        go(-100, 100);
-        delayMs(400);
+        go(-90, 90);
+        delayMs(500);
+        go(50, 50);
+        delayMs(100);
       }
       broj += 1;
     }
-    if (rightFront() < 100) {
-      go(50, 60);
-    }
-    if (leftFront() < 100) {
-      go(60, 50);
-    }
-    if (leftFront() > 150 && rightFront() > 150) {
-      go(60, 60);
+    if (rightFront() < leftFront()) {
+      if (rightFront() < 80) {
+        go(50, 60);
+      }
+    } else if (rightFront() > leftFront()) {
+      if (leftFront() < 80) {
+        go(60, 50);
+      }
+    } else {
+      go(80, 80);
     }
   } else if (!prolazak) {
-    go(50, 50);
+    go(40, 40);
     delayMs(1200);
-    go(-50, - 50);
+    go(-40, - 40);
     delayMs(1200);
     go(100, -100);
-    delayMs(400);
+    delayMs(550);
     prolazak = true;
-  }else if (prolazak){
-    go(100, 100);
-    delayMs(1000);
-  }else {
+  } else if (prolazak && broj != 6) {
+    if (front() < 200) {
+      go(90, -90);
+      delayMs(600);
+      broj = 6;
+    } else {
+      if (rightFront() < leftFront()) {
+        if (rightFront() < 80) {
+          go(50, 60);
+        }
+      } else if (rightFront() > leftFront()) {
+        if (leftFront() < 80) {
+          go(60, 50);
+        }
+      } else {
+        go(80, 80);
+      }
+    }
+  } else if (prolazak && broj == 6) {
+    if (front() > 330 && leftFront() > 300) {
+      go(50, 100);
+      delayMs(1000);
+    } else if (!lineAny()) {
+      go(80, 80);
+    } else {
+      broj = 7;
+    }
+  } else {
     stop();
     end();
   }
 
-  // PRETVORBA VARIJABLE BROJAČA U TEKST
   sprintf(simbol, "%d", broj);
-  // ZADATAK: prikažite simbol
   display(simbol);
 }
